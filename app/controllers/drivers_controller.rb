ec8b6a4ededff15a2003e4888a65d3bed6b54e67
Jeff Nelson <jpnelson85@gmail.com>
2023-05-11 18:59:32 -0500

    add race/driver indexs

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,6 @@
+class DriversController< ApplicationController
+
+  def index
+    @drivers = Driver.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/races', to: 'races#index'
+
+  get '/drivers', to: 'drivers#index'
 end
